e139a27821b0f413181de75d81fa8f1c49286199
pagestarter <pagstarter@gmail.com>
2016-03-04 15:38:48 +0000

    Finish user signup OK

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def update
@@ -17,13 +20,14 @@ class UsersController < ApplicationController
   def delete
   end
   
-  def show
-    @user = User.find(params[:id])
+  def new
+    @user = User.new
   end
   
   private
   
   def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
   end
 end
